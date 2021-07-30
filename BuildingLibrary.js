ce5f8dca45458b9303ab1eced1c2a58445b1bb9f
mikeflo0 <65425755+mikeflo0@users.noreply.github.com>
2021-07-30 08:04:19 -0500

    Add files via upload

---
+++ BuildingLibrary.js
@@ -0,0 +1,88 @@
+class Media {
+  constructor(title){
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  get title() {
+    return this._title
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut
+  }
+  get ratings () {
+    return this._ratings
+  }
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+  toggleCheckOutStatus(){
+    this.isCheckedOut = !this._isCheckedOut;
+  }
+  getAverageRating(){
+    let ratingSum = this.ratings.reduce((accumulator, rating) => 
+    accumulator + rating);
+    return ratingSum / this.ratings.length;
+  }
+  addRating(newRating){
+    this.ratings.push(newRating);
+  }
+}
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author(){
+    return this._author;
+  }
+  get pages(){
+    return this._pages;
+  }
+}
+class Movie extends Media {
+  constructor(title, director, runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get director(){
+    return this._director;
+  }
+  get runTime(){
+    return this._runTime;
+  }
+}
+class CD extends Media{
+  constructor(title, artist, songs){
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+  get artist(){
+    return this._artist;
+  }
+  get songs(){
+    return this._songs;
+  }
+}
+
+
+const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 554);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Speed', 'Jan de Bont', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(5);
+speed.addRating(1);
+speed.addRating(1);
+console.log(speed.getAverageRating());
+
